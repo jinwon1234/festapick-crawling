f2b04679b1eb38d613e2ed747f3e0b57207eb075
jinwon1234 <ljw0626@naver.com>
2025-11-09 21:32:41 +0900

    feat: 울산 축제 크롤러 구현

---
+++ crawler/ulsan_cralwer.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+from domain import models
+from repository import festival_repository
+from datetime import datetime
+
+def getSoup(url):
+    baseUrl = "https://www.ulsanculture.kr"
+    response = requests.get(baseUrl + url)
+    html = response.text
+    return BeautifulSoup(html, 'html.parser')
+
+
+
+def ulsanCrawling():
+    soup = getSoup("/webuser/exhibit/all_list.html?sch_so_show_kind=4")
+    components = soup.select(".gal-box > div > div > a")
+    for component in components: 
+        link = component.attrs['href']
+        soup = getSoup(link)
+        poster = soup.select_one(".img-box > img")['src']
+        title = soup.select_one(".subject").text
+        overView = soup.select_one(".cont").text
+        addr1 = soup.find("dt", string="장소").find_next_sibling("dd").text
+        date = soup.find("dt", string="기간").find_next_sibling("dd").text.split("~")
+        startDate = date[0].strip()
+        endDate = date[1].strip()
+
+        new_festival = models.Festival(
+            addr1=addr1,
+            addr2=None,
+            areaCode=7,
+            contentId=None,
+            createdDate=datetime.now(),
+            updatedDate=datetime.now(),
+            startDate=startDate,
+            endDate=endDate,
+            festivalType="FESTAPICK",
+            state="PROCESSING",
+            homePage=None,
+            posterInfo=poster,
+            title=title,
+            overView=overView,
+            manager_id=None
+        )
+
+        if not festival_repository.existFestivalByTitle(new_festival.title):
+            festival_repository.insertFestival(new_festival)
+        
